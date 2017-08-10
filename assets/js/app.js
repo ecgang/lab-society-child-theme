01dadba7de63d4d180aeac48fe010234542080a9
www-data <www-data@prous274.wsynth.net>
2017-08-09 17:12:18 -0700

    added scroll to top button

--- assets/js/app.js
+++ assets/js/app.js
@@ -229,7 +229,27 @@ $(document).ready(function() {
     	scrollToAnchor('name-question-' + num);
 	});
 
-	
+	if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
 
 	$( '.woocommerce-store-notice__dismiss-link' ).click( function() {
 		Cookies.set( 'store_notice', 'hidden', { path: '/' } );
